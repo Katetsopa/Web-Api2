a4b019d12feb80cedbdddf6c44a86d684cd27c8d
Kateryna Tsopa <Kateryna_Tsopa@epam.com>
2016-11-16 15:00:06 +0200

    new commit

--- SDSK.ADI/Controllers/MailsController.cs
+++ SDSK.ADI/Controllers/MailsController.cs
@@ -1,20 +1,54 @@
-﻿using SDSK.API.Model;
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
+using SDSK.API.Model;
+using SDSK.API.Models;
 
 namespace SDSK.API.Controllers
 {
     public class MailsController : ApiController
     {
-        //GET /api/mails
-        [HttpGet]
-        public Mail Get()
-        {
-            return new Mail() {Id =1 , Body = "mail", Priority = Priority.Low};
-        }
+        //static list of 
 
+        //GET /api/mails
         //GET /api/mails/{id}
         //PUT /api/mails/{id}
         //POST /api/mails
         //DELETER /api/mails/{id}
+
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Mails/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Mails
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Mails/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Mails/5
+        public void Delete(int id)
+        {
+        }
+
     }
 }
---
+++ SDSK.ADI/Models/SDSKAPIContext.cs
@@ -0,0 +1,25 @@
+﻿using System.Data.Entity;
+using SDSK.API.Model;
+
+namespace SDSK.API.Models
+{
+    public class SDSKAPIContext : DbContext
+    {
+        public SDSKAPIContext() : base("name=SDSKAPIContext")
+        {
+            Database.SetInitializer(new SDSKAPIInitializer());
+        }
+
+        public System.Data.Entity.DbSet<SDSK.API.Model.Mail> Mails { get; set; }
+
+    }
+
+    public class SDSKAPIInitializer : DropCreateDatabaseAlways<SDSKAPIContext>
+    {
+        protected override void Seed(SDSKAPIContext context)
+        {
+            context.Mails.Add(new Mail() { Id = 1, Body = "mail1", Priority = Priority.Low, AttachementId = 1 });
+            context.Mails.Add(new Mail() { Id = 2, Body = "mail2", Priority = Priority.High, AttachementId = 2 });
+        }
+    }
+}
